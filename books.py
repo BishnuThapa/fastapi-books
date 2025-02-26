010e387bc3a7843c72ffd818ee7492458aa51945
Bishnu <thapabishnu20@gmail.com>
2025-02-26 13:09:20 +0545

    get all books

--- books.py
+++ books.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
+BOOKS=[
+    {'title':'Title One', 'author':'Author One','category':'Science'},
+    {'title':'Title Two', 'author':'Author Two','category':'Science'},
+    {'title':'Title Three', 'author':'Author Three','category':'History'},
+    {'title':'Title Four', 'author':'Author Four','category':'Math'},
+    {'title':'Title Five', 'author':'Author Five','category':'Math'},
+    {'title':'Title Six', 'author':'Author Two','category':'Math'},
+]
+
+# @app.get("/")
+# async def get_books():
+#     return {"books": ["Python", "Java", "C++"]}
+
+
 @app.get("/books")
-async def get_books():
-    return {"books": ["Python", "Java", "C++"]}
+async def read_all_books():
+    return BOOKS
